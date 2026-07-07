6fefeaf7a91a95aa49b7e0ab2351d1fdd312cc27
andrd-jhon <jv-andrade02@outlook.com>
2026-07-06 22:38:14 -0300

    Criação de Design Factory para corrigir erro de adição de migrations;

---
+++ Ecommerce/Ecommerce.Infrastructure/Context/ApplicationDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Ecommerce.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Ecommerce.Infrastructure.Context
+{
+    public class ApplicationDbContextFactory
+        : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            optionsBuilder.UseMySql(
+                "Server=localhost;DataBase=baseecommerce;Uid=root;Pwd=103005@Mysql",
+                new MySqlServerVersion(new Version(8, 0, 42)));
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
